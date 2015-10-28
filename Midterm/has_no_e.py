c476a6d469b528f68d7b6fb7090d63b0a5b98748
Amber <amber.simmons@uoit.net>
2015-10-28 09:42:22 -0400

    adding Midterm

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,9 @@
+python has_no_e()
+
+filename=input
+
+if  "e" in filename:
+	print('False')
+    else:
+	print('True')
+	
---
+++ Midterm/strip.sh
@@ -0,0 +1,10 @@
+#! bash
+bash
+#Amber Simmons 100520774
+
+print --cat"k="= <k>
+print --cat"m="= <m>
+print --cat"filename="= <filename>
+
+cat filename | head-n -m | tail -n +k
+
